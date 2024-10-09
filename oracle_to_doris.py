1c183d731eacf80ab6e02bae1fd27023a8c670c9
yuhexiong <yuhexiong871210@gmail.com>
2024-10-09 09:31:07 +0800

    feat: oracle to doris

---
+++ oracle_to_doris.py
@@ -0,0 +1,42 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder \
+    .appName("MySparkApp") \
+    .config("spark.some.config.option", "config-value") \
+    .getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
+
+# read oracle
+df = spark.read \
+    .format("jdbc") \
+    .option("url", "jdbc:oracle:thin:@host:port:service") \
+    .option("dbtable", "schema.table") \
+    .option("user", "user") \
+    .option("password", "password") \
+    .load()
+
+df.show(10)
+
+processed_df = df \
+    .select(
+        col("BOOK_ID").alias("book_id"),
+        col("TITLE").alias("title"),
+        col("AUTHOR").alias("author"),
+        col("PUBLICATION_YEAR").alias("publication_year"),
+        col("GENRE").alias("genre"),
+        col("RATING").alias("rating"),
+        col("STATUS").alias("status")
+    )
+
+# save doris
+ds = processed_df \
+    .write \
+    .mode("append") \
+    .format("doris") \
+    .option("checkpointLocation", "./checkpoint") \
+    .option("doris.table.identifier", "database.table") \
+    .option("doris.fenodes", "host:port") \
+    .option("user", "user") \
+    .option("password", "password") \
+    .save()
